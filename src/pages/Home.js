af789bfb993ae81f352277ce5f82de66b2b4a982
Jweb60 <jy.babiarczyk@gmail.com>
2022-06-27 18:57:42 +0200

    mouse effect

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Mouse from '../components/Mouse';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation />
+      <Mouse />
+    </div>
+  );
+};
+
+export default Home;<h1>HOME</h1>
