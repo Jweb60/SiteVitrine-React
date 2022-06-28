2850ebcf8b696609aad8822facdc7d3f29fce78f
Jweb60 <jy.babiarczyk@gmail.com>
2022-06-28 17:44:18 +0200

    socials icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Mouse from '../components/Mouse';
 import Navigation from '../components/Navigation';
+import SocialNetworks from '../components/SocialNetworks';
 
 const Home = () => {
   return (
     <div>
       <Navigation />
       <Mouse />
+      <SocialNetworks />
     </div>
   );
 };
