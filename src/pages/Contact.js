19e4f4c6c83440d844f402758c3baa3e8e46e8e1
Jweb60 <jy.babiarczyk@gmail.com>
2022-06-29 17:02:19 +0200

    contact ok

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import Mouse from '../components/Mouse';
-import Navigation from '../components/Navigation';
-import Logo from '../components/Logo';
-import ContactForm from '../components/ContactForm';
-
+import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import ContactForm from "../components/ContactForm";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import SocialNetworks from "../components/SocialNetworks";
 
 const Contact = () => {
   return (
@@ -13,7 +14,47 @@ const Contact = () => {
         <Navigation />
         <Logo />
         <ContactForm />
-        
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>Adresse</h4>
+              <p>24 Rue du Commandant Guilbaud</p>
+              <p>75016 Paris</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>Téléphone</h4>
+              <CopyToClipboard text="0627965588" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Téléphone copié!")}
+                >
+                  06.06.06.06.06
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard text="fsagency@gmail.com" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("email copié!")}
+                >
+                  fsagency@gmail.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetworks />
+          <div className="credits">
+            Jean-Yves B. - 2022
+          </div>
+        </div>
       </div>
     </main>
   );
